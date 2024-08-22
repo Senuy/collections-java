2af64e8b6fce71f9b17e4a446566216644b9e53f
yunes <yunes.santos@PMCG.IMTI>
2024-08-22 09:52:05 -0400

    finalizando ordenaçao List

--- collectionsJavaApi/src/list/Ordenacáo/OrdenacaoPessoas/OrdenacaoPessoas.java
+++ collectionsJavaApi/src/list/Ordenacáo/OrdenacaoPessoas/OrdenacaoPessoas.java
@@ -1,7 +1,12 @@
 package list.Ordenacáo.OrdenacaoPessoas;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import list.Ordenacáo.OrdenacaoPessoas.*;
+
+import list.Pesquisa.CatalogoLivros.main;
 
 public class OrdenacaoPessoas {
 
@@ -17,9 +22,32 @@ public class OrdenacaoPessoas {
 		pessoalist.add(new Pessoa(nome, idade, altura));
 	}
 	
-	public void ordenar
+	public List<Pessoa> ordenarPorIdade(){
+		List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoalist);
+		Collections.sort(pessoasPorIdade);
+		return pessoasPorIdade;
+	}
+	
+	public List<Pessoa> ordenarPorAltura(){
+		List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoalist);
+		Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+		return pessoasPorAltura;
+	}
 	
 	
-	
+	public static void main(String[] args) {
+		OrdenacaoPessoas ordenacaoPessoa = new OrdenacaoPessoas();
+		
+		ordenacaoPessoa.adicionarPessoa("Yunes", 29, 1.75);
+		ordenacaoPessoa.adicionarPessoa("Sara", 22, 1.50);
+		ordenacaoPessoa.adicionarPessoa("Guilherme", 20, 1.80);
+		ordenacaoPessoa.adicionarPessoa("Gumercinda", 50, 1.70);
+		ordenacaoPessoa.adicionarPessoa("Jiacomo", 29, 1.75);
+		ordenacaoPessoa.adicionarPessoa("Tereza", 44, 1.55);
+		
+		System.out.println(ordenacaoPessoa.ordenarPorIdade());
+		System.out.println("----------------------------------------------------");
+		System.out.println(ordenacaoPessoa.ordenarPorAltura());
+	}
 	
 }
--- collectionsJavaApi/src/list/Ordenacáo/OrdenacaoPessoas/Pessoa.java
+++ collectionsJavaApi/src/list/Ordenacáo/OrdenacaoPessoas/Pessoa.java
@@ -1,16 +1,24 @@
 package list.Ordenacáo.OrdenacaoPessoas;
 
-public class Pessoa implements Comparable {
-	
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
+
 	private String nome;
 	private int idade;
 	private double altura;
-	
+
 	public Pessoa(String nome, int idade, double altura) {
 		super();
 		this.nome = nome;
 		this.idade = idade;
 		this.altura = altura;
+	}
+
+	@Override
+	public int compareTo(Pessoa p) {
+
+		return Integer.compare(idade, p.getIdade());
 	}
 
 	public String getNome() {
@@ -29,13 +37,19 @@ public class Pessoa implements Comparable {
 	public String toString() {
 		return String.format("Pessoas [nome=%s]", nome);
 	}
-
-	@Override
-	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 	
 	
 
 }
+
+	class ComparatorPorAltura implements Comparator<Pessoa>{
+
+		@Override
+		public int compare(Pessoa p1, Pessoa p2) {
+	
+			return Double.compare(p1.getAltura(), p2.getAltura());
+		}
+		
+	}
+
+
