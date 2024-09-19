c60f66900294b978d7cc460929604de7cf00389d
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2024-09-19 16:20:20 -0400

    finalizando exercício que fico pra trás

--- collectionsJavaApi/src/list/ordenacao/OrdenacaoNumero/OrdenacaoNumeros.java
+++ collectionsJavaApi/src/list/ordenacao/OrdenacaoNumero/OrdenacaoNumeros.java
@@ -1,7 +1,9 @@
 package list.ordenacao.OrdenacaoNumero;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 
 public class OrdenacaoNumeros {
 	
@@ -15,11 +17,35 @@ public class OrdenacaoNumeros {
 		numeroList.add(new Numero(numero));
 	}
 	
-	public void ordenarAscendente() {
-		
+	public List<Numero> ordenarAscendente() {
+		List<Numero> NumAscendente = new ArrayList<>(numeroList);
+		Collections.sort(NumAscendente);
+		return NumAscendente;
 	}
 	
-	
-	
+	public List<Numero> ordenarDescedente(){
+		List <Numero> NumDescendente =  new ArrayList<>(numeroList);
+		Collections.sort(NumDescendente.reversed());
+		return NumDescendente;
+	}
 
+	public static void main(String[] args) {
+		
+		
+		OrdenacaoNumeros ordNum = new OrdenacaoNumeros();
+		ordNum.adicionarNumero(1);
+		ordNum.adicionarNumero(78378);
+		ordNum.adicionarNumero(1343);
+		ordNum.adicionarNumero(2);
+		ordNum.adicionarNumero(5);
+		ordNum.adicionarNumero(3);
+		ordNum.adicionarNumero(4);
+		
+		
+		System.out.println(ordNum.ordenarAscendente());
+		System.out.println(ordNum.ordenarDescedente());
+		
+		
+	}
 }
+
